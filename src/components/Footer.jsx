d9a1a38523082b8f356050e2eea497903089a415
diegoogprado <diegoogprado@hotmail.com>
2024-08-17 10:11:42 -0300

    ajuste icones

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,15 @@ export default function Footer() {
         <footer id="contact" className="footer">
             <div className="contactF">
                 <div className="contact1">
-                    <img src="../../public/assets/envelope.png" alt="" />
+                    <img src="/assets/envelope.png" alt="" />
                     <p>contato@agenciamare.com.br</p>
                 </div>
                 <div className="contact1">
-                    <img src="../../public/assets/phone.png" alt="" />
+                    <img src="/assets/phone.png" alt="" />
                     <p>+55 21 3334-4444</p>
                 </div>
                 <div className="contact1">
-                    <img src="../../public/assets/map.png" alt="" />
+                    <img src="/assets/map.png" alt="" />
                     <p>Penha - Rio de Janeiro, RJ</p>
                 </div>
             </div>
